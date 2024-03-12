5d058f5a76083b28eae311c6bbbeab0e81931b2d
LiRaMINGI <liramingi@gmail.com>
2024-03-12 20:41:44 +0200

    add comments

--- Laba3/3laba_2_v2.py
+++ Laba3/3laba_2_v2.py
@@ -1,32 +1,36 @@
 def f(S):
-    SL=""  #Буду
-    Ch = len(S)
+    SL=""  #Будущая строчка
     c='0123456789'
     NS=S+'**'
     Z=[]; A=[]
 
-    for n in range(len(NS)):
+    for n in range(len(NS)):  #В цикле проставляю одиночным буквам цифру 1
         if NS[n] not in c and NS[n+1] not in c:
             NS=NS.replace(NS[n],NS[n]+'1',1)
+
     AS=NS[:-2]
-    for i in range(len(NS)):
+
+    for i in range(len(NS)):  #Заменяю все буквы на #
         if NS[i] not in c:
             NS=NS.replace(NS[i],'#',1)
-    N = NS.split('#')
-    for d in N:
+
+    N = NS.split('#') #Убираю #
+
+    for d in N: #Закидываю цифры в массив
         if d!='':
             Z.append(int(d))
 
-    for j in range(len(AS)):
+    for j in range(len(AS)):  #Заменяю все цифры на ?
         if AS[j] in c:
             AS = AS.replace(AS[j], '?', 1)
 
-    SS = AS.split('?')
-    for b in SS:
+    SS = AS.split('?') #Убираю ?
+
+    for b in SS: #Закидываю в массив
         if b!='':
             A.append(b)
 
-    if len(A) == len(Z):
+    if len(A) == len(Z): #Если кол-во букв и цифр совпадают, то ок
         for m in range(len(A)):
             SL+=A[m]*Z[m]
         return SL
